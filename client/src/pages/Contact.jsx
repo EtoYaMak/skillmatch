335ee61624cd411ba1edde1ddd7be618abd5a96b
Mak <itsmak98@gmail.com>
2023-07-17 08:50:06 +0100

    updated Contact Page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -47,18 +47,17 @@ const Contact = () => {
 
   return (
     <div className="max-w-7xl mx-auto">
-      <section className="text-gray-700 body-font">
-        <div className="container px-5 py-5 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+      <section className="text-gray-400 body-font font-Inter">
+        <div className="container px-5 py-5 mx-auto bg-zinc-800 rounded-xl">
+          <div className="flex flex-col text-center w-full mb-12 bg-inherit">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-50 bg-inherit font-Inter">
               Contact Us
             </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify.
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base bg-inherit">
+              Connect with Skillmint Team by Sending us a Message!
             </p>
           </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="lg:w-1/2 md:w-2/3 mx-auto bg-inherit">
             {success && (
               <p className="text-green-500 mb-4">Message sent successfully!</p>
             )}
@@ -67,12 +66,15 @@ const Contact = () => {
                 Error occurred. Please try again.
               </p>
             )}
-            <form onSubmit={handleSubmit} className="flex flex-wrap m-2">
-              <div className="p-2 w-full sm:w-1/2">
-                <div className="relative">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-wrap m-2 bg-inherit"
+            >
+              <div className="p-2 w-full sm:w-1/2 bg-inherit">
+                <div className="relative bg-inherit">
                   <label
                     htmlFor="name"
-                    className="leading-7 text-md text-zinc-600"
+                    className="leading-7 text-md text-zinc-200 bg-inherit font-Inter font-semibold"
                   >
                     Name
                   </label>
@@ -86,11 +88,11 @@ const Contact = () => {
                   />
                 </div>
               </div>
-              <div className="p-2 w-full sm:w-1/2">
-                <div className="relative">
+              <div className="p-2 w-full sm:w-1/2 bg-inherit">
+                <div className="relative bg-inherit">
                   <label
                     htmlFor="email"
-                    className="leading-7 text-md text-zinc-600"
+                    className="leading-7 text-md text-zinc-200 bg-inherit font-Inter font-semibold"
                   >
                     Email
                   </label>
@@ -104,11 +106,11 @@ const Contact = () => {
                   />
                 </div>
               </div>
-              <div className="p-2 w-full">
-                <div className="relative">
+              <div className="p-2 w-full bg-inherit">
+                <div className="relative bg-inherit">
                   <label
                     htmlFor="message"
-                    className="leading-7 text-md text-zinc-600"
+                    className="leading-7 text-md text-zinc-200 bg-inherit font-Inter font-semibold"
                   >
                     Message
                   </label>
@@ -121,10 +123,10 @@ const Contact = () => {
                   ></textarea>
                 </div>
               </div>
-              <div className="p-2 w-full">
+              <div className="p-2 w-full bg-inherit">
                 <button
                   type="submit"
-                  className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg font-Inter font-semibold"
                   disabled={loading}
                 >
                   {loading ? "Sending..." : "Submit"}
@@ -132,55 +134,15 @@ const Contact = () => {
               </div>
             </form>
 
-            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-              <Link className="text-indigo-500">example@email.com</Link>
-              <p className="leading-normal my-5">
-                49 Smith St.
+            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-600 text-center bg-inherit">
+              <Link className="text-gray-200 text-lg">
+                skillmintofficial@gmail.com
+              </Link>
+              <p className="leading-normal my-5 bg-inherit">
+                {/*                 49 Smith St.
                 <br />
-                Saint Cloud, MN 56301
+                Saint Cloud, MN 56301 */}
               </p>
-              <span className="inline-flex">
-                <Link className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </Link>
-                <Link className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </Link>
-                <Link className="ml-4 text-gray-500">
-                  <svg fill="none" className="w-5 h-5" viewBox="0 0 24 24">
-                    <rect
-                      width="20"
-                      height="20"
-                      x="2"
-                      y="2"
-                      rx="5"
-                      ry="5"
-                    ></rect>
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                  </svg>
-                </Link>
-                <Link className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </Link>
-              </span>
             </div>
           </div>
         </div>
