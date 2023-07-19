ba8e2da25e78224ecae9c88f97f8d45c3ee0921b
Mak <itsmak98@gmail.com>
2023-07-19 12:33:38 +0100

    employer Dash CSS done

--- client/src/pages/UserDash.jsx
+++ client/src/pages/UserDash.jsx
@@ -55,46 +55,57 @@ function UserDash() {
   return (
     <div className="flex bg-transparent justify-center p-4">
       <div className="bg-inherit w-full max-w-screen-md py-6">
-        <h1 className="text-2xl font-semibold text-center">
+        <h1 className="text-2xl font-semibold text-center text-[#d4d7d7] font-Inter select-none">
           Welcome, {user.name}
         </h1>
         {jobs.length > 0 ? (
           <>
-            <p className="text-xl text-center text-gray-600 uppercase p-2">
+            <p className="text-xl text-center text-gray-400 font-Inter uppercase p-2 select-none">
               Your job posts
             </p>
             <div className="mt-2">
               {jobs.map((job) => (
                 <div
                   key={job._id}
-                  className="Main Tile w-full py-2 bg-zinc-900 hover:bg-gray-700 rounded-lg my-2"
+                  className="Main Tile w-full bg-black/40 hover:bg-black/90 rounded-lg my-2"
                 >
-                  <div className="Second flex flex-col md:flex-row justify-between items-center px-1 py-4 bg-inherit">
+                  <div className="Second flex flex-col md:space-y-0  md:flex-row  md:justify-between w-full md:items-center px-1 py-4 bg-transparent">
                     {/* JobCard */}
-                    <Link to={`/jobs/${job._id}`}>
-                      <div className="ThirdOne flex items-center px-2 space-x-6 bg-inherit">
-                        <img
-                          src={job.logo}
-                          alt={job.position}
-                          className="w-20 h-20 rounded-full"
-                        />
-                        <div className="bg-transparent">
-                          <h2 className="text-xl font-bold  w-64 bg-inherit text-sky-600">
-                            {job.position}
-                          </h2>
-                          <h3 className="text-lg text-gray-400 bg-inherit">
-                            {job.company}
-                          </h3>
+                    <Link
+                      to={`/jobs/${job._id}`}
+                      className="w-[80%] bg-transparent  "
+                    >
+                      <div className="ThirdOne flex flex-row space-x-2 justify-evenly  w-full bg-transparent">
+                        <div className="flex bg-transparent items-center w-full">
+                          <div className="bg-transparent object-contain w-fit p-2 ">
+                            <img
+                              src={job.logo}
+                              alt={job.position}
+                              className="bg-transparent object-cover md:w-28 md:h-28 h-[90px] w-[90px] rounded-sm  "
+                            />
+                          </div>
+
+                          <div className="bg-transparent w-full p-2">
+                            <h2 className="text-xl font-bold tracking-wider w-fit bg-inherit text-[#d0333c]">
+                              {job.position}
+                            </h2>
+                            <h3 className="text-lg tracking-wider font-semibold text-[#d4d7d7] bg-inherit">
+                              {job.company}
+                            </h3>
+                          </div>
                         </div>
                       </div>
                     </Link>
                     {/* JobCard End*/}
 
-                    <div className="flex  w-1/2 justify-evenly bg-inherit">
+                    <div className="flex w-full md:w-1/2 justify-around md:justify-evenly items-center bg-transparent">
                       {/* TIME */}
                       <div className="ThirdTwo flex flex-row md:flex-row items-center space-x-2 bg-transparent">
-                        <FaClock className="text-gray-200 bg-inherit" />
-                        <span className="text-md text-gray-200 bg-inherit">
+                        <FaClock
+                          className="text-[#d4d7d7] bg-inherit"
+                          size={24}
+                        />
+                        <span className="text-lg text-[#d4d7d7] bg-inherit">
                           {calculateTimeSinceJobWasPosted(
                             currentDate,
                             job.createdAt
@@ -106,29 +117,25 @@ function UserDash() {
                       {/* COG WHEEL */}
                       <div className="ThirdThree flex items-center bg-inherit pr-5">
                         <button
-                          className="cog-wheel bg-blue-500 hover:bg-blue-800 font-semibold rounded-full w-12 h-12 flex items-center justify-center "
+                          className="cog-wheel bg-[#aba6a6]/80 hover:bg-[#d0333c]  rounded-full w-12 h-12 flex items-center justify-center "
                           onClick={() => toggleDropdown(job._id)}
                         >
-                          <FaCog className="w-5 h-5 text-white bg-transparent" />
+                          <FaCog
+                            size={24}
+                            className="text-[#fff] bg-transparent pointer-events-none"
+                          />
                         </button>
                         {dropdownOpen[job._id] && (
-                          <div className="dropdown-menu absolute mt-[10em] sm:ml-[3.2em]  bg-zinc-300 rounded shadow-sm shadow-gray-400/50 text-white p-2 animate-fadeIn ">
-                            {/*                             <Link
-                              to={`/jobs/${job._id}/update`}
-                              className="dropdown-option flex items-center rounded-md py-2 px-4 w-full text-gray-800 hover:bg-gray-600 hover:text-white hover:font-semibold transition duration-300"
-                            >
-                              <FaEdit className="mr-2 w-5 h-5 bg-transparent" />
-                              Edit
-                            </Link> */}
+                          <div className="dropdown-menu absolute mt-[10em] sm:ml-[1em]  bg-[#1c1f21]/70 backdrop-blur-sm rounded shadow-sm  text-white p-2 animate-fadeIn ">
                             <button
-                              className="dropdown-option flex items-center rounded-md py-2 px-4 w-full text-gray-800 hover:bg-gray-600 hover:text-white hover:font-semibold transition duration-300"
+                              className="dropdown-option flex items-center rounded-md py-2 px-4 w-full text-[#fff] hover:bg-black/80 hover:text-[#d0333c] hover:font-semibold transition duration-300"
                               onClick={() => handleDeleteOption(job._id)}
                             >
                               <FaTrash className="mr-2 w-5 h-5 bg-transparent" />
                               Delete
                             </button>
                             <button
-                              className="dropdown-option flex items-center rounded-md py-2 px-4 w-full text-gray-800 hover:bg-gray-600 hover:text-white hover:font-semibold transition duration-300"
+                              className="dropdown-option flex items-center rounded-md py-2 px-4 w-full text-[#fff] hover:bg-black/80 hover:text-[#d0333c] hover:font-semibold transition duration-300"
                               onClick={() => handleCloseOption(job._id)}
                             >
                               <FaTimes className="mr-2 w-5 h-5 bg-transparent" />
@@ -154,9 +161,10 @@ function UserDash() {
         <div className="flex justify-center mt-6">
           <Link
             to="/post"
-            className="py-2 px-4 text-lg font-medium bg-blue-500 text-white rounded hover:bg-blue-900 hover:text-white hover:font-semibold transition-colors duration-200 ease-in-out"
+            className="btn btn-outline text-[#fff]/80 border-[#d0333c] hover:bg-[#d0333c] hover:border-[#d4d7d7] hover:text-[#d4d7d7]
+                   flex mx-auto text-xl font-Inter tracking-widest w-[10em] transition-colors duration-200 ease-in-out"
           >
-            Click Here to Post a {jobs.length > 0 ? "new" : ""} Job
+            Post {jobs.length > 0 ? "A" : ""} Job
           </Link>
         </div>
       </div>
