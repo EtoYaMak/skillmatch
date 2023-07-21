5a5eff47648c320fea7f4b486a6f95a49e87adde
Mak <itsmak98@gmail.com>
2023-07-22 00:32:51 +0100

    cssday2.1

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 // Making HTTP request
 import axios from "axios";
 
-const API_URL = "http://35.178.166.193/api/users/";
+const API_URL = "http://35.178.166.193:4000/api/users/";
 /* const API_URL = "http://localhost:4000/api/users/"; */
 
 // Register User
