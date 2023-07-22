4915e21d5281935ca532b2d03bf3aba7b33e2008
Mak <itsmak98@gmail.com>
2023-07-22 01:08:37 +0100

    cssday2.2

--- client/src/features/contact/contactService.js
+++ client/src/features/contact/contactService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const sendContactForm = async (formData) => {
   try {
     const response = await axios.post(
-      "http://35.178.166.193:4000/api/contact",
+      "http://35.178.166.193/api/contact",
       formData
     );
     return response.data;
