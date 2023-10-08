196e6d2226e653e9e69256a49a8c48c91cfdc58e
Mak <itsmak98@gmail.com>
2023-10-08 01:11:01 +0100

    USER +privacy policy +password_validation 1/2. TODO terms and conditions USER & Student

---
+++ client/src/components/level_2/PrivacyPolicyModal.jsx
@@ -0,0 +1,224 @@
+// PrivacyPolicyModal.js
+
+import React from "react";
+
+const PrivacyPolicyModal = ({ isOpen, onClose }) => {
+  const modalStyles = {
+    display: isOpen ? "block" : "none",
+    position: "fixed",
+    zIndex: 9999, // Ensure it's above everything else
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "white",
+    padding: "20px",
+    borderRadius: "4px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+  };
+
+  const overlayStyles = {
+    display: isOpen ? "block" : "none",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.7)", // Darkened background
+    zIndex: 9998, // Below the modal
+  };
+  return (
+    <div
+      className={`inset-0 fixed flex items-center justify-center bg-white w-1/2 h-fit font-Inter ${
+        isOpen ? "block" : "hidden"
+      }`}
+    >
+      <div className="modal-container" style={overlayStyles}>
+        <div
+          className="modal-content h-1/2 overflow-y-scroll overflow-x-clip py-4 border-4 border-red-600 "
+          style={modalStyles}
+        >
+          <h2 className="font-bold text-xl">Privacy Policy</h2>
+          <p className="mt-1 font-semibold">Last Updated: 7 Oct 2023</p>
+          <p className="mt-1">
+            This Privacy Policy describes how Skillmint ("we," "us," or "our")
+            collects, uses, discloses, and protects the personal information of
+            users of our job portal website located at
+            <a
+              href="https:\\www.skillmint.io"
+              target="_blank"
+              className="underline font-bold mx-1"
+            >
+              Skillmint
+            </a>
+            (the "Website"). By using our Website, you consent to the practices
+            described in this Privacy Policy.
+          </p>
+          <div className="my-2">
+            <h3 className="font-bold">1. Information We Collect</h3>
+
+            <p>
+              We collect the following types of personal information from users
+              of our Website:
+            </p>
+          </div>
+          <div className="ml-4 my-4">
+            <p>
+              <strong>User Registration:</strong>
+            </p>
+            <ul>
+              <li>Name</li>
+              <li>Email</li>
+              <li>Password</li>
+            </ul>
+          </div>
+          <div className="ml-4 my-4">
+            <p>
+              <strong>Job Listing:</strong>
+            </p>
+            <ul>
+              <li>Job Title</li>
+              <li>Job Location</li>
+              <li>Job Setting Type (e.g., on-site, remote, full-time, etc.)</li>
+              <li>Job Description</li>
+            </ul>
+          </div>
+          <div className="ml-4 my-1">
+            <p>
+              <strong>Job Applicants:</strong>
+            </p>
+            <ul>
+              <li>Applicant Name</li>
+              <li>Applicant Email</li>
+              <li>Applicant CV</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">2. How We Use Your Information</h3>
+
+            <p>We use the information we collect for the following purposes:</p>
+
+            <ul>
+              <li>To create and manage user accounts.</li>
+              <li>To enable users to post job listings and apply for jobs.</li>
+              <li>
+                To communicate with users regarding job listings, applications,
+                and account-related matters.
+              </li>
+              <li>To improve our Website and services.</li>
+              <li>To comply with legal obligations.</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">3. Sharing Your Information</h3>
+
+            <p>
+              We may share your personal information with the following third
+              parties:
+            </p>
+
+            <ul>
+              <li>
+                Other users of the Website as necessary for job applications and
+                listings.
+              </li>
+              <li>
+                Service providers who assist us in operating our Website and
+                providing services.
+              </li>
+              <li>
+                Legal authorities when required by law or to protect our rights.
+              </li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">4. Security</h3>
+
+            <p>
+              We implement appropriate security measures to protect your
+              personal information. However, no data transmission over the
+              internet or storage system can be guaranteed to be 100% secure.
+            </p>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">5. Your Rights</h3>
+
+            <p>
+              You have the following rights regarding your personal information:
+            </p>
+
+            <ul>
+              <li>
+                Access: You can request access to the personal information we
+                hold about you.
+              </li>
+              <li>
+                Rectification: You can request the correction of inaccurate or
+                incomplete personal information.
+              </li>
+              <li>
+                Deletion: You can request the deletion of your personal
+                information under certain circumstances.
+              </li>
+              <li>
+                Restriction: You can request the restriction of the processing
+                of your personal information.
+              </li>
+              <li>
+                Portability: You can request the transfer of your personal
+                information to another service provider.
+              </li>
+            </ul>
+
+            <p>
+              To exercise these rights, please contact us using the contact
+              information provided in Section 7.
+            </p>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">6. Cookies and Tracking</h3>
+
+            <p>
+              We may use cookies and similar tracking technologies to collect
+              information about your use of our Website. You can control cookies
+              through your browser settings.
+            </p>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">7. Contact Us</h3>
+
+            <p>
+              If you have any questions, concerns, or requests regarding this
+              Privacy Policy or the handling of your personal information,
+              please contact us at:{" "}
+              <a
+                href="mailto:skillmintofficial@gmail.com"
+                className="font-semibold underline"
+              >
+                skillmintofficial@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className="my-4">
+            <h3 className="font-bold">8. Changes to this Privacy Policy</h3>
+
+            <p>
+              We may update this Privacy Policy from time to time. Any changes
+              will be posted on this page, and the date of the latest revision
+              will be indicated.
+            </p>
+          </div>
+          <div className="flex flex-row gap-4 justify-center items-center">
+            <button
+              className="btn btn-ghost bg-red-600 text-white"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicyModal;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,36 +6,64 @@ import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { register, reset } from "../features/auth/authSlice";
 import Spinner from "../components/Spinner";
+import PrivacyPolicyModal from "../components/level_2/PrivacyPolicyModal";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 function Register() {
+  //FormData
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     password2: "",
   });
-
   const { name, email, password, password2 } = formData;
+  const passwordsMatch = password === password2;
+
+  //hooks
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  //Redux State
   const { user, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
+  //State Hooks
+  const [isPrivacyPolicyOpen, setPrivacyPolicyOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
   useEffect(() => {
     if (isSuccess) {
       toast.success("Please check your email to activate your account");
       navigate("/");
     }
   }, [isSuccess, navigate]);
+  //Funcs
+  const isPasswordValid = (password) => {
+    // Implement your password validation logic here
+    // Example: Return true if the password meets your criteria
+    return password.length >= 8; // Minimum length of 8 characters
+  };
+  const openPrivacyPolicy = () => {
+    setPrivacyPolicyOpen(true);
+  };
 
+  const closePrivacyPolicy = () => {
+    setPrivacyPolicyOpen(false);
+  };
+
+  //onChange form fields
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
-
+  //Submit
+  const privacyPolicyCheckbox = document.getElementById(
+    "privacy-policy-checkbox"
+  );
+  /* 
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
@@ -49,14 +77,48 @@ function Register() {
 
       dispatch(register(userData));
     }
+  }; */
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const privacyPolicyCheckbox = document.getElementById(
+      "privacy-policy-checkbox"
+    );
+
+    if (privacyPolicyCheckbox.checked) {
+      if (!passwordsMatch) {
+        toast.error("Passwords do not match");
+      } else if (!isPasswordValid(password)) {
+        toast.error("Password does not meet validation criteria");
+      } else {
+        const userData = {
+          name,
+          email,
+          password,
+        };
+
+        dispatch(register(userData));
+      }
+    } else {
+      // Display a message to the user indicating that they need to accept the Privacy Policy.
+      toast.error("Please accept the Privacy Policy before registering.");
+    }
   };
 
+  //
   if (isLoading) {
     return <Spinner />;
   }
 
   return (
     <>
+      <div className="h-full">
+        <PrivacyPolicyModal
+          isOpen={isPrivacyPolicyOpen}
+          onClose={closePrivacyPolicy}
+        />
+      </div>
+
       <div className="min-h-screen max-h-fit h-full w-full mx-auto">
         <div className="flex flex-col md:flex-row justify-evenly items-center mt-10 gap-4  mx-auto max-w-[960px]">
           {/* Navigate through forms*/}
@@ -102,6 +164,7 @@ function Register() {
                   value={name}
                   placeholder="Enter your name"
                   onChange={onChange}
+                  required
                 />
               </div>
 
@@ -117,6 +180,7 @@ function Register() {
                   value={email}
                   placeholder="Enter your email"
                   onChange={onChange}
+                  required
                 />
               </div>
 
@@ -124,31 +188,107 @@ function Register() {
                 <label className="text-white text-2xl font-Inter p-2 bg-inherit">
                   Password
                 </label>
-                <input
-                  type="password"
-                  className="form-control input bg-white/5 text-xl text-white focus:outline-none focus:shadow-outline focus:border-none focus:ring-[#d0333c]"
-                  id="password"
-                  name="password"
-                  value={password}
-                  placeholder="Enter password"
-                  onChange={onChange}
-                />
+                <div className="flex items-center">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="form-control input bg-white/5 text-xl text-white focus:outline-none focus:shadow-outline focus:border-none focus:ring-[#d0333c] flex-1"
+                    id="password"
+                    name="password"
+                    value={password}
+                    placeholder="Enter password"
+                    onChange={onChange}
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="toggle-password-button ml-2 text-white"
+                  >
+                    {showPassword ? (
+                      <AiFillEyeInvisible size={24} />
+                    ) : (
+                      <AiFillEye size={24} />
+                    )}
+                  </button>
+                </div>
               </div>
 
               <div className="form-group pb-2 grid bg-inherit">
                 <label className="text-white text-2xl font-Inter p-2 bg-inherit">
                   Confirm Password
                 </label>
-                <input
-                  type="password"
-                  className="input bg-white/5 text-xl text-white focus:outline-none focus:shadow-outline focus:border-none focus:ring-[#d0333c]"
-                  id="password2"
-                  name="password2"
-                  value={password2}
-                  placeholder="Confirm password"
-                  onChange={onChange}
-                />
+                <div className="flex items-center">
+                  <input
+                    type={showPassword2 ? "text" : "password"}
+                    //className="input bg-white/5 text-xl text-white focus:outline-none focus:shadow-outline focus:border-none focus:ring-[#d0333c] flex-1"
+                    className={`form-control input  bg-white/5 text-xl text-white  ${
+                      password !== "" && password2 !== "" && !passwordsMatch
+                        ? "ring-[#d0333c]"
+                        : "ring-green-500"
+                    } flex-1`}
+                    id="password2"
+                    name="password2"
+                    value={password2}
+                    placeholder="Confirm password"
+                    onChange={onChange}
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword2(!showPassword2)}
+                    className="toggle-password-button ml-2 text-white"
+                  >
+                    {showPassword2 ? (
+                      <AiFillEyeInvisible size={24} />
+                    ) : (
+                      <AiFillEye size={24} />
+                    )}
+                  </button>
+                </div>
               </div>
+              {/* Password matching feedback */}
+              {password !== "" && password2 !== "" && !passwordsMatch && (
+                <div className="password-mismatch-feedback text-white underline underline-offset-2 decoration-red-600">
+                  Passwords do not match
+                </div>
+              )}
+
+              {/* Password validation feedback */}
+              {password !== "" && !isPasswordValid(password) && (
+                <div className="password-validation-feedback text-white underline underline-offset-2 decoration-red-600">
+                  Password does not meet validation criteria
+                </div>
+              )}
+              {/* Priacy Policy Confirm */}
+              <div className="flex flex-row">
+                <label
+                  for="privacy-policy-checkbox"
+                  className="flex gap-1 flex-row items-center text-white"
+                >
+                  <input
+                    type="checkbox"
+                    id="privacy-policy-checkbox"
+                    name="privacy-policy-checkbox"
+                    className="checkbox checkbox-sm checkbox-success"
+                  />
+                  <p>I accept the</p>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      openPrivacyPolicy();
+                    }}
+                    className="text-red-600 underline"
+                  >
+                    Privacy Policy
+                  </a>
+                  <p>and</p>
+                  <a href="" target="_blank" className="text-red-600 underline">
+                    Terms & Conditions
+                  </a>
+                </label>
+              </div>
+              {/* Register Button */}
               <div className="form-group w-max mx-auto bg-inherit my-2">
                 <button
                   type="submit"
@@ -158,6 +298,7 @@ function Register() {
                   Register
                 </button>
               </div>
+              {/* Login?  */}
               <div className="flex flex-col justify-center items-center bg-inherit">
                 <span className="mt-2 text-xl font-boldgap-2 bg-transparent text-white font-Inter pb-2">
                   Already have an account?
